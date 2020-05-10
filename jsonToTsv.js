af1933e580fa9a4fb77c8e9270e2623e7cfed30c
Piyush Ahuja <piyushahuja.in@gmail.com>
2020-05-10 07:43:14 +0100

    Added script for converting json to tsv file

---
+++ jsonToTsv.js
@@ -0,0 +1,48 @@
+
+
+// This script can be used to convert a json to tsv. Please modify the source data. 
+
+const fs = require('fs')
+var data = require('./data.json')
+let destination = './data.tsv'
+
+
+
+let line = "";
+
+for (var [key] of Object.entries(data[0])){
+    line = line + key + '\t'
+}
+
+line = line + '\n'
+
+fs.appendFile(destination, line, (err) => {
+    if (err) throw err;
+    console.log("Appended: ", line)
+})
+
+
+newData = data.splice(1)
+console.log("Data", newData)
+
+for (var row of newData){
+    console.log("Row: " , row);
+
+    let line = ""
+
+    for (const [key, value] of Object.entries(row)){
+        line = line + value + '\t'
+    }
+
+    line = line + "\n"
+
+    fs.appendFile(destination, line, (err) => {
+    if (err) throw err;
+    console.log("Appended: ", line)
+})
+
+}
+
+
+
+
